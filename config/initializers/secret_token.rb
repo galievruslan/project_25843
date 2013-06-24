1b14f7e4c82380363344361e1b0fc1835885b256
Ruslan Galiev <galievruslan@gmail.com>
2013-06-24 10:29:20 +0400

    Create Project 25843

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project25843::Application.config.secret_token = 'acd304fa07f59ee89f68ec04091e0242e1a2397a580683192b3ebaaa10eb39cf38437ba5ae4d419871a4ed68cc27b3debea1ba9d2415bf5d006fa7a98c9d2439'
